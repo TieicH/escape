2566de4565fc215b52f6a28a296701811d4a0523
ivy <unjust@gmail.com>
2021-07-07 21:24:12 -0500

    new fotos

--- src/js/data.js
+++ src/js/data.js
@@ -1,10 +1,12 @@
 const key = "AIzaSyAJ-S12P0yrxM_jOYAH74HIMiST5JTbsMQ"
-const sid="19ttl2WeUfe8-fp05lLgl_Mk1kpBwbkVS4nTLu-hHJQE";
+const sid_orig="1RVReP21W0xfHf-Z82jO-flozDv2qAgpQj2OVwi-GyqE"
+// const sid="19ttl2WeUfe8-fp05lLgl_Mk1kpBwbkVS4nTLu-hHJQE";
 const rowStart = 2;
 const colStart = "A";
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${sid}/values/Sheet1!`;
+const colEnd = "F";
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${sid_orig}/values/Sheet1!`;
 const start = `${colStart}${rowStart}`;
-const end = "F";
+const end = `${colEnd}`;
 
 const columnKeys = {
   "name": 0,
